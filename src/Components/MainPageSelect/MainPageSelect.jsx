e9191b6ee1a3fae753a72203ecaa7d6805769a83
SilverSol <ivan.soloviov@nure.ua>
2023-06-09 17:53:50 +0300

    hotfix

--- src/Components/MainPageSelect/MainPageSelect.jsx
+++ src/Components/MainPageSelect/MainPageSelect.jsx
@@ -17,7 +17,7 @@ const SelectComponent = (props) => {
   }, [mode, dispatch]);
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box className="selectSize">
       <FormControl fullWidth variant="filled">
         <InputLabel id="demo-simple-select-label">Show ...</InputLabel>
         <Select
